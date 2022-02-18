0a5af922bffa671abd9cde847102962c2a3f486e
namGyunKim <skarbs01@gmail.com>
2022-02-19 02:38:25 +0900

    카테고리로 가게 추가

--- src/main/java/com/example/finalproject/Controller/ClothesController.java
+++ src/main/java/com/example/finalproject/Controller/ClothesController.java
@@ -1,10 +1,22 @@
 package com.example.finalproject.Controller;
 
+import com.example.finalproject.repository.ClothesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ClothesController {
 
+    @Autowired
+    ClothesRepository clothesRepository;
 
+
+    @RequestMapping("clothes/kate/{kate}")
+    public String clothesKate(@PathVariable(value = "kate",required = false)String kate){
+
+
+        return "mustache/clothes/clothesKate";
+    }
 }
